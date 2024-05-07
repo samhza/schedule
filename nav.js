01ce7b9b0e86d8ac9bda6701aa8a72ad054af4e5
Samuel Hernandez <sam@samhza.com>
2024-05-07 01:01:44 -0400

    cache courses when unchanged

--- nav.js
+++ nav.js
@@ -1,6 +1,5 @@
-import { readFileSync } from "fs";
+import { writeFileSync, readFileSync } from "fs";
 import { Parse } from './parse.js';
-
 const exempt = new Set(['01:119:115', '01:198:111', '01:640:151', '01:355:101', '01:119:116', '01:119:117'])
 function checkPrereqs(cs, prereqNotes, has) {
     if(exempt.has(cs)) return true;
@@ -25,7 +24,8 @@ function hasPrereqs(prereqs, has) {
     if(prereqs.constructor.name == 'AllOf'){
         return prereqs.reqs.every(r=>hasPrereqs(r, has));
     };
-    process.exit(1);
+    console.log("TODO: " + prereqs.constructor.name);
+    return process.exit(1);
 }
 function isEqOrGr(cs, req) {
     if(cs.slice(0,6) != req.slice(0,6)) return false;
@@ -35,15 +35,39 @@ function isMathElective(cs) {
     return cs.startsWith("01:640") && cs[7]>='3';
 }
    
-let fallCourses = JSON.parse(readFileSync("fall23.json"));
-let springCourses = JSON.parse(readFileSync("spring24.json"));
-let summerCourses = JSON.parse(readFileSync("summer.json"));
-let courses = fallCourses.concat(springCourses).concat(summerCourses);
-let courseMap = new Map();
-courses.forEach(c=>courseMap.set(c.courseString, c));
-let fallSet = new Set(fallCourses.map(c=>c.courseString));
-let springSet = new Set(springCourses.map(c=>c.courseString));
-let summerSet = new Set(summerCourses.map(c=>c.courseString));
+let courses, courseMap, fallCourses, springCourses, fallSet, springSet;
+let fresh = false;
+try {
+    ({ fallCourses, springCourses } = JSON.parse(readFileSync("cached.json")));
+    courses = fallCourses.concat(springCourses);
+    courseMap = new Map();
+    courses.forEach(c=>courseMap.set(c.courseString, c));
+    fallSet = new Set(fallCourses.map(c=>c.courseString));
+    springSet = new Set(springCourses.map(c=>c.courseString));
+} catch(error) {
+    console.log(error)
+    readFresh();
+}
+
+function getCourse(cs) {
+    let course = courseMap.get(cs)
+    if (course || fresh) return course;
+    readFresh();
+    return getCourse(cs);
+}
+
+function readFresh() {
+    fresh = true;
+    fallCourses = JSON.parse(readFileSync("fall24.json"));
+    springCourses = JSON.parse(readFileSync("spring24.json"));
+    courses = fallCourses.concat(springCourses);
+    courseMap = new Map();
+    courses.forEach(c=>courseMap.set(c.courseString, c));
+    fallSet = new Set(fallCourses.map(c=>c.courseString));
+    springSet = new Set(springCourses.map(c=>c.courseString));
+}
+
+
 const csElectives = new Set(["01:198:210", "01:198:213", "01:198:214", "01:198:314", "01:198:323", "01:198:324", "01:198:334", "01:198:336", "01:198:352", "01:198:411", "01:198:415", "01:198:416", "01:198:417", "01:198:419", "01:198:424", "01:198:425", "01:198:428", "01:198:431", "01:198:437", "01:198:439", "01:198:440", "01:198:442", "01:198:443", "01:198:444", "01:198:452", "01:198:460", "01:198:461", "01:198:462", "01:198:493", "01:198:494", "14:332:376", "14:332:423", "14:332:424", "14:332:443", "14:332:451", "14:332:452", "14:332:453", "14:332:456", "14:332:472", "01:640:338", "01:640:348", "01:640:354", "01:640:428", "01:640:454", "01:640:461", "01:730:315", "01:730:407", "01:730:408", "01:730:329", "01:730:424", "01:615:441", "01:960:384", "01:960:463", "01:960:476", "01:960:486"])
 const contents = readFileSync(process.argv[2] ?? "courses.txt");
 const lines = String(contents).split("\n").filter(l=>!l.startsWith("#")).values();
@@ -64,7 +88,7 @@ while(true){
             break;
         }
         const cs = value.split(" ")[0];
-        const c = courseMap.get(cs);
+        const c = getCourse(cs);
         if(!c){
             missing.push(cs);
             continue;
@@ -74,8 +98,30 @@ while(true){
     terms.push({name: value, values: term})
 }
 const allCourses = Array.from(new Set(terms.flatMap(t=>t.values)));
+
+if(fresh){
+    const toCache = new Set(csElectives);
+    allCourses.forEach(toCache.add, toCache);
+    let data = {
+	fallCourses: [],
+	springCourses: [],
+    }
+    for(const course of toCache){
+	let crs = getCourse(course);
+	if(!crs) continue;
+	if(fallSet.has(course)){
+	    data.fallCourses.push(crs);
+	}
+	if(springSet.has(course)){
+	    data.springCourses.push(crs);
+	}
+    }
+
+    writeFileSync("cached.json", JSON.stringify(data));
+};
+
 for(const [i, courseString] of allCourses.entries()){
-    const course = courseMap.get(courseString);
+    const course = getCourse(courseString);
     if(!course){
         throw new Error("unknown course string: " + courseString)
     }
@@ -93,15 +139,14 @@ for(const [i,val] of terms.entries()){
     const set = {
         "fall": fallSet,
         "spring": springSet,
-        "summer": summerSet,
     }[szn];
     if(!set) continue;
-    for(const lack in crs.filter(c=>!set.has(c))){
+    for(const lack of crs.filter(c=>!set.has(c))){
         console.log(`${lack} isn't offered in the ${szn}`);
     }
     const has = terms.slice(0,i).flatMap(t=>t.values);
     for(const courseString of crs){
-        const course = courseMap.get(courseString);
+	const course = getCourse(courseString);
         if(!checkPrereqs(courseString, course.preReqNotes, has)) {
             console.log(`${courseString} ${course.title.trim() ?? course.expandedTitle.trim()} missing prereqs:`);
             console.log(`   ${course.preReqNotes}`)
@@ -111,9 +156,9 @@ for(const [i,val] of terms.entries()){
 
 for(const {name,values} of terms){
     const foo = new Map();
-    console.log(`${name.padStart(10, " ")} - ${values.map(c=>courseMap.get(c).credits).reduce((a,b)=>a+b) + (foo.get(name) ?? 0)} credits`)
+    console.log(`${name.padStart(10, " ")} - ${values.map(c=>getCourse(c).credits).reduce((a,b)=>a+b) + (foo.get(name) ?? 0)} credits`)
 }
-console.log(allCourses.map(cs=>courseMap.get(cs).credits).reduce((a,v)=>a+v)+4+9 + "/120 credits")
+console.log(allCourses.map(cs=>getCourse(cs).credits).reduce((a,v)=>a+v)+4+9 + "/120 credits")
 console.log(allCourses.filter(cs=>csElectives.has(cs)).length + "/7 CS electives")
 console.log(allCourses.filter(cs=>csElectives.has(cs) && cs.startsWith("01:198")).length + "/5 of which are in the CS department")
 console.log(allCourses.filter(cs=>isMathElective(cs)).length + "/8 math electives")
